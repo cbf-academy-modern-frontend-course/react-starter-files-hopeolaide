4ff4649f0b5fbf1e38861ae2dcb6bc550a39ea78
Hope Olaide <hopeolaide@gmail.com>
2022-11-11 06:57:54 -0500

    Update styling.

--- react-webpage/index.js
+++ react-webpage/index.js
@@ -1,27 +1,18 @@
-"use strict";
+("use strict");
 
 function ClickableButton() {
     const [clicked, setClick] = React.useState(false);
 
     if (clicked) {
-        return "Nothing here yet! Change this text to add more info.";
+        return "Hello World!";
     }
-
     return React.createElement(
         "button",
         {
-            style: {
-                backgroundColor: "#3392e4",
-                color: "#fff",
-                border: "0",
-                padding: "10px 20px",
-                fontWeight: "bold",
-                borderRadius: "5px",
-                fontSize: "1.1em",
-            },
+            className: "main-button",
             onClick: () => setClick(true),
         },
-        "Click To Reveal"
+        "Press Here"
     );
 }
 
