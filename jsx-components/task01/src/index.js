34b2b680677cf5a2c54a8123a86e7fc9556606e7
Hope Olaide <hopeolaide@gmail.com>
2022-11-11 20:12:48 -0500

    modified: index.js

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const name = '[YOUR NAME GOES HERE]';
+const name = "Hope";
 
-function Profile(name){
-    return <main id="content" role="main" className="base">
-            <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
+function Profile(name) {
+    if (/name/i) {
+        return (
+            <main id="content" role="main" className="base">
+                <h1>{name}'s React Page</h1>
+                <p>Example of react JSX in action</p>
             </main>
+        );
+    }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Profile(name));
 
+// The syntax /learn react/i is special
+// syntax called a regular expression.
+// The ‘i’ signifies that the text ‘learn react’
+// can be upper or lower case
